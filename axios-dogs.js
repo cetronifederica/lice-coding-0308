84578551b7b2d7179eb49b4bbea1525a26a7d6c5
cetronifederica <cetronifederica97@gmail.com>
2022-08-03 12:35:12 +0200

    aggiungere una foto di un cane alla gallery random

--- axios-dogs.js
+++ axios-dogs.js
@@ -1,9 +1,38 @@
+// quando clicco sul bottone faccio una chiamata alla API 
+const addBtn = document.getElementById('add');
+console.log(addBtn);
+addBtn.addEventListener('click', addDog);
+
+
+//funzione che aggiunge un immagine di cane alla gallery
+function addDog(){
+//console.log('aggiungo un cane');
+//con axios chiamo l'api dei cani e prendo l'url dell'immagine
 axios.get('https://dog.ceo/api/breeds/image/random')
   .then(function (response) {
     // handle success
-    console.log(response);
+    //navigo l'oggetto facendo uscire in console solo il messaggio con http dell'immagine 
+    console.log(response.data.message);
+    const imgUrl = response.data.message;
+    // con l'url dell'immagine creo un nuovo tag img da mettere in pagina
+    createImg(imgUrl);
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   });
+}
+
+// aggiunge alla gallery una img con la src passata come parametro
+function createImg(url){
+let image = document.createElement('img');
+image.src = url;
+image.className = 'img-fluid';
+image.alt = 'foto di un cane';
+//dopo aver creat l'oggetto appendo la foto alla pagina
+document.getElementById('gallery').appendChild(image);
+}
+
+
+
+
