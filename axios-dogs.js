a2458fd022093b5cf2e14603944b349302ecef56
cetronifederica <cetronifederica97@gmail.com>
2022-08-03 12:00:26 +0200

    inizio axios

---
+++ axios-dogs.js
@@ -0,0 +1,9 @@
+axios.get('https://dog.ceo/api/breeds/image/random')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
